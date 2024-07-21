de334384c6fac698f4d1b34403f373ca4e2f7638
Maxim Olesov <windigotp@gmail.com>
2024-07-21 17:21:56 +0800

    feat: add config file

---
+++ src/config.py
@@ -0,0 +1,23 @@
+from dotenv import find_dotenv, load_dotenv
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    MODE: str
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file='../.env')
+
+
+load_dotenv(find_dotenv('../.env'))
+settings = Settings()
