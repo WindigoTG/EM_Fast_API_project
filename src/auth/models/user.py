cf5b8112715e1b5f1cc1adcaa7923e8f158c2be0
Maxim Olesov <windigotp@gmail.com>
2024-07-21 17:30:41 +0800

    feat: add user orm model

--- src/auth/models/user.py
+++ src/auth/models/user.py
@@ -1,5 +1,11 @@
-from src.models import BaseModel
+from sqlalchemy.orm import Mapped
+
+from src.models import BaseModel, custom_types
 
 
 class User(BaseModel):
-    ...
+    __tablename__ = "user"
+
+    id: Mapped[custom_types.uuid_pk_T]
+    first_name: Mapped[custom_types.str_50_T]
+    last_name: Mapped[custom_types.str_50_T]
