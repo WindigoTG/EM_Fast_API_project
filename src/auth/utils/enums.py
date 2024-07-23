c92465c61547c505936153a949b0dd3ee1d753c4
Maxim Olesov <windigotp@gmail.com>
2024-07-23 22:27:38 +0800

    feat: add auth utils

---
+++ src/auth/utils/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class AuthServiceOperationResultEnum(Enum):
+    SUCCESS = 0
+    ACCOUNT_DOES_NOT_EXIST = 1
+    ACCOUNT_ALREADY_EXISTS = 2
+    ACCOUNT_NOT_VERIFIED = 3
+    ACCOUNT_ALREADY_VERIFIED = 4
+    ACCOUNT_IN_USE = 5
+    WRONG_TOKEN = 6
+    COMPANY_DOES_NOT_EXIST = 7
