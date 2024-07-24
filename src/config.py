960f2caf67af5fb1955892a5045afc5cbd89d428
Maxim Olesov <windigotp@gmail.com>
2024-07-24 23:22:15 +0800

    feat: update config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,11 @@
-from dotenv import find_dotenv, load_dotenv
+from pathlib import Path
 
+from dotenv import find_dotenv, load_dotenv
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 class Settings(BaseSettings):
@@ -12,9 +17,24 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_NAME: str
 
+    PRIVATE_KEY_NAME: str
+    PUBLIC_KEY_NAME: str
+    CERTS_DIRECTORY_NAME: str
+    ALGORITHM: str = "RS256"
+    ACCESS_TOKEN_LIFETIME: int = 15
+
+    @property
+    def PRIVATE_KEY_PATH(self):
+        return BASE_DIR / self.CERTS_DIRECTORY_NAME / self.PRIVATE_KEY_NAME
+
+    @property
+    def PUBLIC_KEY_PATH(self):
+        return BASE_DIR / self.CERTS_DIRECTORY_NAME / self.PUBLIC_KEY_NAME
+
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
 
     model_config = SettingsConfigDict(env_file='../.env')
 
